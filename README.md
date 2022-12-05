d5700df7a19b9c73e5303e038159fe6a90205bf1
Emmanuel Rudelle <ADKL-MAC096@MBP-de-admin.lan>
2022-12-05 19:13:21 +0100

    init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Talk data Le Wagon
+
+Support pour la présentation "Se préparer à rejoindre une équipe data"
+## Setup
+
+```
+pip install -r requirements.txt
+```
---
+++ others/test_pylance.py
@@ -0,0 +1,8 @@
+def sum_list(list1, list2):
+    very_very_very_very_very_very_very_very_long1 = []
+    very_very_very_very_very_very_very_very_long2 = []
+    add = (
+        very_very_very_very_very_very_very_very_long1
+        + very_very_very_very_very_very_very_very_long2
+    )
+    add
